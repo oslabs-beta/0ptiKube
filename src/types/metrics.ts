2656e0fe6f2c88ef9a8de1d5f69c0519233263b1
Richard Rivera <richard.rivera.roman@gmail.com>
2025-02-21 13:41:21 -0500

    Extract types from components

---
+++ src/types/metrics.ts
@@ -0,0 +1,28 @@
+// Shape for CPU or Memory usage results
+export type PromVectorData = {
+  resultType: string;
+  result: {
+    metric: {
+      labels: {
+        pod?: string;
+      };
+    };
+    value: {
+      time: string;
+      value: number;
+    };
+  }[];
+};
+
+// Shape of the historical data results
+export type PrometheusMatrixResponse = {
+  resultType: string;
+  result: Array<{
+    metric: {
+      labels: {
+        pod?: string;
+      };
+    };
+    values: Array<{ time: string; value: number }>;
+  }>;
+};
