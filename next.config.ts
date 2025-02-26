297cf998e5ff71a5628d58b13e1af301a79e2e3a
Oliver Antonio Fajardo <oliverafajardoucb@gmail.com>
2025-02-26 13:13:12 -0800

    yaml files for minikube updated

--- next.config.ts
+++ next.config.ts
@@ -5,16 +5,12 @@ const nextConfig = {
     domains: ['avatars.githubusercontent.com']
   },
   experimental: {
-    serverActions: true,
+    serverActions: {
+      allowedOrigins: ['localhost:3000']
+    }
   },
-  // Only need these essential settings for Ingress
   httpAgentOptions: {
     keepAlive: true,
-  },
-  // Simple server config to bind to all interfaces for Ingress
-  server: {
-    hostname: '0.0.0.0',
-    port: 3000
   }
 }
 
